66797776717b7f71b33947b10f67929447482cd1
Teinishi <79356047+Teinishi@users.noreply.github.com>
2026-08-22 18:25:32 +0900

    core のサブパッケージを分けたことによるエラーを修正

--- packages/dev-viewer/vite.config.ts
+++ packages/dev-viewer/vite.config.ts
@@ -1,15 +1,9 @@
-import { fileURLToPath, URL } from "node:url";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: {
-      "@sw-file-lib/core": fileURLToPath(new URL("../core/src/index.ts", import.meta.url)),
-      "@sw-file-lib/xml": fileURLToPath(new URL("../xml/src/index.ts", import.meta.url)),
-      "@sw-file-lib/three": fileURLToPath(new URL("../three/src/index.ts", import.meta.url)),
-    },
+    conditions: ["development"],
   },
 });
