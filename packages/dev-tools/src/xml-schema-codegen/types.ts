9fbcab83e9afb2c0819acd906d3eb99f5c24589b
Teinishi <79356047+Teinishi@users.noreply.github.com>
2026-09-15 18:51:03 +0900

    不要コードを削除

--- packages/dev-tools/src/xml-schema-codegen/types.ts
+++ packages/dev-tools/src/xml-schema-codegen/types.ts
@@ -1,6 +1,3 @@
-import path from "node:path";
-import type ts from "typescript";
-import type { GenerateOptions } from ".";
 import { SetMap } from "./utils";
 
 export type FileKind = "schema" | "mutableInterface" | "immutableInterface";
@@ -10,45 +7,6 @@ export interface FilePaths {
   schema: string;
   mutableInterface: string;
   immutableInterface: string;
-}
-
-export interface FileAnalyzeContext {
-  checker: ts.TypeChecker;
-  sourceFile: ts.SourceFile;
-  filePaths: FilePaths;
-  typeAliasImportMap: SetMap<string, string>;
-  docLinkImport: {
-    schema: SetMap<string, string>;
-    mutableInterface: SetMap<string, string>;
-    immutableInterface: SetMap<string, string>;
-  };
-  schemaCodeEntries: string[];
-  immutableInterfaceCodeEntries: string[];
-}
-
-export function newContext(
-  stem: string,
-  checker: ts.TypeChecker,
-  sourceFile: ts.SourceFile,
-  options: GenerateOptions,
-): FileAnalyzeContext {
-  return {
-    checker,
-    sourceFile,
-    filePaths: {
-      schema: path.join(options.outDir, `${stem}-schema.ts`),
-      mutableInterface: sourceFile.fileName,
-      immutableInterface: path.join(options.outDir, `${stem}-immutable.ts`),
-    },
-    typeAliasImportMap: new SetMap(),
-    docLinkImport: {
-      schema: new SetMap(),
-      mutableInterface: new SetMap(),
-      immutableInterface: new SetMap(),
-    },
-    schemaCodeEntries: [],
-    immutableInterfaceCodeEntries: [],
-  };
 }
 
 export type IdentifierSchemaInfo = {
